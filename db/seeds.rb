95730da12701792aae37aba12c634ff7e39e4f0c
Mike <ruecktmi@gmail.com>
2019-08-07 15:26:06 -0700

    end 188

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Mike", password: "password")
+User.create(username: "Sam", password: "password")
+User.create(username: "Pete", password: "password")
+User.create(username: "Mom", password: "password")
+ Message.create(body: "Hi", user: User.first)
+   Message.create(body: "Eating", user: User.last)
+    Message.create(body: "Just had steak", user: User.last)
+     Message.create(body: "Nice", user: User.first)
+      Message.create(body: "Mine was better", user: User.first)
